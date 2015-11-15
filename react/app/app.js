d368a65339261dc8e67864e1377b2e201c56d05c
Guten Ye <ywzhaifei@gmail.com>
2015-11-15 23:27:54 +0800

    complete react 60%

--- react/app/app.js
+++ react/app/app.js
@@ -1,34 +1,66 @@
 import React from "react"
 import ReactDom from "react-dom"
-import Model from "./model"
-import Input from "./input"
-import List from "./list"
-import Statusbar from "./statusbar"
+import {Todos} from "./common/todos"
+import _ from "lodash-guten"
+import cx from "classnames"
+global._ = _
+global.cx = cx
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
+var App = React.createClass({
+  getInitialState() {
+    return {
+      todos: null
+    }
+  },
+
+  componentWillMount() {
     window.app = this
-  }
+    Todos.find().then(todos => {
+      this.setState({todos: todos})
+    })
+  },
 
   render() {
+    var {todos} = this.state
+    if (!todos)
+      return <div />
     return (
-      <section>
-        <header>
-          <Input />
-        </header>
-        <section>
-          <List />
-        </section>
-        <footer>
-          <Statusbar />
-        </footer>
-      </section>
-    )
-  }
-}
+      <div>
+        <div>
+          <input type="checkbox" onChange={todos.toggleAllCompleted} />
+          <input is="gut-input" placeholder="What needs to be done?" autofocus on-enter="enter" on-esc="esc" />
+        </div>
 
-window.model = new Model()
+        <ul>
+          {todos.items.map(item =>
+          <li className={cx({completed: item.completed})}>
+            <input type="checkbox" checked={item.completed} onChange={this.completedChanged.bind(this, item)}></input>
+            <input value={item.title} onChange={this.titleChanged}></input>
+            <button onClick={this.delete}>X</button>
+          </li>
+          )}
+        </ul>
+
+        <flex>
+          <div>{todos.activeCount} items left</div>
+          <div>All Active Completed</div>
+          <div>Clear completed</div>
+        </flex>
+      </div>
+    )
+  },
+
+  completedChanged(item, e) {
+    item.completed = e.target.checked
+    item.save()
+  },
+
+  titleChanged(item, e) {
+    item.title = e.target.value
+    item.save()
+  },
+})
+
 window.rerender = function() {
   app.setState({})
 }
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
   entry: path.join(__dirname, "app/app.js"),
   output: {path: path.join(__dirname, "dist"), filename: "app.js" },
   module: {
-    loaders: [{test: /\.js$/, loader: "babel?presets[]=es2015&presets[]=react", include: path.join(__dirname, "app")}]
+    loaders: [{test: /\.js$/, loader: "babel?presets[]=es2015&presets[]=react", exclude: /node_modules/}]
   }
 }
